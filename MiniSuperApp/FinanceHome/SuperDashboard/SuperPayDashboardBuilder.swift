d15c3f5eaff7e9d4926078b889a58a1322f21dbb
Iannealer <duwjdtn@ajou.ac.kr>
2022-03-19 17:01:33 +0900

    [Update] SuperPayDashboardInteractor

--- MiniSuperApp/FinanceHome/SuperDashboard/SuperPayDashboardBuilder.swift
+++ MiniSuperApp/FinanceHome/SuperDashboard/SuperPayDashboardBuilder.swift
@@ -8,12 +8,12 @@
 import ModernRIBs
 
 protocol SuperPayDashboardDependency: Dependency {
-  // TODO: Declare the set of dependencies required by this RIB, but cannot be
-  // created by this RIB.
+  var balance: ReadOnlyCurrentValuePublisher<Double> { get }
 }
 
-final class SuperPayDashboardComponent: Component<SuperPayDashboardDependency> {
-  
+final class SuperPayDashboardComponent: Component<SuperPayDashboardDependency>,
+                                          SuperPayDashboardInteractorDependency {
+  var balance: ReadOnlyCurrentValuePublisher<Double> { dependency.balance }
   // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
 }
 
@@ -32,7 +32,10 @@ final class SuperPayDashboardBuilder: Builder<SuperPayDashboardDependency>, Supe
   func build(withListener listener: SuperPayDashboardListener) -> SuperPayDashboardRouting {
     let component = SuperPayDashboardComponent(dependency: dependency)
     let viewController = SuperPayDashboardViewController()
-    let interactor = SuperPayDashboardInteractor(presenter: viewController)
+    let interactor = SuperPayDashboardInteractor(
+      presenter: viewController,
+      dependency: component
+    )
     interactor.listener = listener
     return SuperPayDashboardRouter(interactor: interactor, viewController: viewController)
   }
